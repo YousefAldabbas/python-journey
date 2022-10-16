37eb4244101274450e1171e9ecf21d3923d6b19e
yousef aldabbas <yousefaldabbas0@gmail.com>
2022-10-16 11:55:16 +0300

    2.5

---
+++ classDecorator.py
@@ -0,0 +1,52 @@
+class SimpleDecorator:
+    def __init__(self, own_function):
+        self.func = own_function
+
+    def __call__(self, *args, **kwargs):
+        print(f'{self.func.__name__} was called with the following arguments')
+        print(f'\t{args}\n\t{kwargs}\n')
+        self.func(*args, **kwargs)
+        print('Decorator is still operating')
+
+
+@SimpleDecorator
+def combiner(*args, **kwargs):
+    print("\tHello from the decorated function; received arguments:", args, kwargs)
+
+
+combiner('a', 'b', exec='yes')
+
+'''
+###################################################################################
+
+'''
+class WarehouseDecorator:
+    def __init__(self, material):
+        self.material = material
+
+    def __call__(self, own_function):
+        def internal_wrapper(*args, **kwargs):
+            print('<strong>*</strong> Wrapping items from {} with {}'.format(own_function.__name__, self.material))
+            own_function(*args, **kwargs)
+            print()
+        return internal_wrapper
+
+
+@WarehouseDecorator('kraft')
+def pack_books(*args):
+    print("We'll pack books:", args)
+
+
+@WarehouseDecorator('foil')
+def pack_toys(*args):
+    print("We'll pack toys:", args)
+
+
+@WarehouseDecorator('cardboard')
+def pack_fruits(*args):
+    print("We'll pack fruits:", args)
+
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
---
+++ different-faces/differentFaces.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__(self, vin):
+        print('Ordinary __init__ was called for', vin)
+        self.vin = vin
+        self.brand = ''
+
+    @classmethod
+    def including_brand(cls, vin, brand):
+        print('Class method was called')
+        _car = cls(vin)
+        _car.brand = brand
+        return _car
+
+car1 = Car('ABCD1234')
+car2 = Car.including_brand('DEF567', 'NewBrand')
+
+print(car1.vin, car1.brand)
+print(car2.vin, car2.brand)
